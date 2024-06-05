e475ad3b122c239c548f0ba73f09e14d4cdb7e30
chadgothelf <cbgothelf@gmail.com>
2024-06-05 15:27:00 -0400

    card implementation ( unfinished)

--- deck.py
+++ deck.py
@@ -6,6 +6,7 @@ class Card:
         self.suit = suit
         self.number = number
 
+
 class Deck:
     def __init__(self):
         self.cards = list()
@@ -13,16 +14,37 @@ class Deck:
         for suit in suits:
             for i in range(2, 15):
                 self.cards.append(Card(suit, i))
+        self._removedCards = []
 
     def remove_card(self, card):
         self.cards.remove(card)
+        self._removedCards.append(card)
         pass
     def deal_cards(self, num):
         cards = []
         np.random.shuffle(self.cards)
         for i in range(num):
             x = random.choice(self.cards)
-            cards.append[x]
+            cards.append(x)
             self.remove_card(x)
         return cards
     def choose_cards(self):
+        pass
+
+    def shuffle(self):
+        if len(self._removedCards) == 0:
+            np.random.shuffle(self.cards)
+        else:
+            for i in self._removedCards:
+                self.cards.append(i)
+            self._removedCards = []
+            np.random.shuffle(self.cards)
+
+    def __str__(self):
+        deck = ""
+        deck += f'CARDS INSIDE: {len(self.cards)}'
+        for i in self.cards:
+            deck += f'Suit: {i.suit} - Number: {i.number}\n'
+
+        return deck
+
